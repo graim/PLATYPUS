c42b12f58560f6de040551c45b03c4fc2c04183c
Kiley Graim <kgraim@gmail.com>
2019-03-22 16:28:44 -0400

    Commit because Friday

--- PLATYPUS_Example/example_Lapatinib_3views.R
+++ PLATYPUS_Example/example_Lapatinib_3views.R
@@ -1,21 +1,60 @@
-#!/usr/bin/env Rscript
-#SBATCH -p ccb --qos=ccb -N3 --exclusive -o ./logs/CCLE_PLATYPUS_Example.%j.out -e ./logs/CCLE_PLATYPUS_Example.%j.err
+## TODO: Build this up into a full example of how to take 1 data set and create interpreted views from it
+## So create the interpreted data, run function to do parameter sweep & create config file, then use that in an MVL run
+
+###############
+## Initial data (provided with example)
+######
+
+## Load libraries
+library(devtools)
+install_github('graim/PLATYPUS')
+library(PLATYPUS)
+
+## Baseline view data: TODO 
+
+## Labels to use: TODO
+
+###############
+## Create a new interpreted view
+######
+
+# Convert the PanCanAtlas data to the same views
+# TODO: Flesh this out
+#generate.feature.data.summary(gene.sample.data, feature.gene.matrix, type = 'expression', value = 'median', num.gene.threshold = 5)
+
+###############
+## Create configuration files for platypus using already created views 
+## Create a config file for a set of tasks and views (e.g. data)
+######
+
+## Views to use
+fns.data <- c(
+  '2015-06-20_DrugTargetsExpression.txt', 
+  '2015-07-02_TissueByMetabolicEnzymeExpression.txt', 
+  '2015-09-08_DrugTargetNearestNeighboursPathway_HallmarkAllExpression.txt', 
+  '2015-07-20_AllSummaryMetrics.txt', 
+  'CCLE_expression_5000_featurematrix.tab', 
+  'DrugTargetPathway_HallmarkPathway_Lapatinib_configFile.txt' 
+)
+fns.data <- file.path('data',fns.data)
+
+## Task file - each column is 1 task. Requires 1 or more tasks
+fn.tasks <- 'CCLE_responselabel_binary_3cat_Lapatinib.tab'
+fn.tasks <- file.path('data',fn.tasks)
+
+## Generate config files - returns a list of the config filenames for using in platypus calls
+config.files.en <- gen.config(fns.data[1:3], fn.tasks, model.type='en', delim='\t', config.loc='config') # elastic net configs
+config.files.rf <- gen.config(fns.data[4:length(fns.data)], fn.tasks, model.type='rf', delim='\t', config.loc='config') # random forest configs
+config.files    <- c(config.files.en, config.files.rf)
 
 ###############
 ## Run platypus using provided data
 ######
 
-## Load functions
-source('../scripts/platypus.basicFunctions.R')
-source('../scripts/platypus.R')
-source('../scripts/llv.platypus.R')
-source('../scripts/cv.platypus.R')
-print('Scripts loaded.'); flush.console()
-
-## Set up an MVL call for the 3 view Lapatinib
 # rf, rf, rf, en
-config.files <- list('DrugTargets_Lapatinib_configFile.txt','DrugTargetPathway_HallmarkPathway_Lapatinib_configFile.txt','MetabolicEnzymes_Lapatinib_configFile.txt','AllSummaryMetrics_Lapatinib_configFile.txt')
-config.files <- file.path('config_CCLE', config.files)
+# TODO: Update this to have 1 baseline view, 1 interpreted view, 1 RF view, and 1 EN view. Any combo of those things
+config.files <- list('DrugTargets_Lapatinib_configFile.txt','DrugTargetPathway_HallmarkPathway_Lapatinib_configFile.txt','MetabolicEnzymes_Lapatinib_configFile.txt')
+config.files <- file.path('config', config.files)
 
 print('Views in use:'); flush.console()
 print(config.files)
@@ -23,7 +62,7 @@ print(config.files)
 ## Set parameters
 n.iters  <- 10                                             # Run for this many iterations of label learning
 m.thresh <- 95                                             # Threshold for learning a label (0-1 range)
-of.name  <- 'platypus_output_CCLE'                         # Directory where results should be stored
+of.name  <- 'platypus_output'                         # Directory where results should be stored
 fn.labs  <- 'CCLE_responselabel_binary_3cat_Lapatinib.tab' # Filename of the labels being predicted
 
 ## Call multiview learning function
@@ -39,53 +78,7 @@ llv.platypus.res <- llv.platypus(fn.views=config.files,fn.labs=fn.labs,no.iterat
 print('Running cv')
 cv.platypus.res <- cv.platypus(fn.views=config.files,fn.labs=fn.labs,no.iterations=n.iters,majority.threshold.percent=m.thresh,output.folder=of.name,expanded.output=TRUE)
 
-## TODO: Build this up into a full example of how to take 1 data set and create interpreted views from it
-## So create the interpreted data, run function to do parameter sweep & create config file, then use that in an MVL run
-
 ## After running LLV and CV, generate the PLATYPUS performance plots
-source('../scripts/plot.llv.R')
-source('../scripts/plot.cv.R')
-plot.llv( fn.labs, 'platypus_output_CCLE' ) # Plot label learning
-plot.cv(fn.labs, 'platypus_output_CCLE') # Plot cross validation - these plots pair with the llv plot
-
-###############
-## Create configuration files for platypus using already created views 
-## Create a config file for a set of tasks and views (e.g. data)
-######
-
-## Load scripts
-source('../scripts/gen_config.R')
-source('../scripts/single_predictor.R')
-
-## Views to use
-fns.data <- c(
-  '2015-06-20_DrugTargetsExpression.txt', # en
-  '2015-07-20_AllSummaryMetrics.txt', # en
-  'CCLE_clinical_featurematrix.tab', # en TODO: this is the old version, not used in PSB results. still has the 'NOT.' features
-  '2015-07-02_TissueByMetabolicEnzymeExpression.txt', # en
-  '2015-09-08_DrugTargetNearestNeighboursPathway_HallmarkAllExpression.txt', # en
-  'CCLE_expression_5000_featurematrix.tab', # en
-  'DrugTargetPathway_HallmarkPathway_Lapatinib_configFile.txt' # rf
-)
-
-## Task file - each column is 1 task. Requires 1 or more tasks
-fn.tasks <- 'CCLE_responselabel_binary_3cat_Lapatinib.tab'
-
-## Append the correct location for the files above
-fn.tasks <- file.path('data_CCLE',fn.tasks)
-fns.data <- file.path('data_CCLE',fns.data)
-
-## Generate config files - returns a list of the config filenames for using in platypus calls
-config.files <- gen.config(fns.data, fn.tasks, model.type='en', delim='\t', config.loc='config_CCLE')
-config.files <- gen.config(fns.data, fn.tasks, model.type='rf', delim='\t', config.loc='config_CCLE')
-
-## Using the configs we've generated, run platypus
-platypus.res <- platypus(fn.views=config.files,fn.labs=fn.tasks)
-
-###############
-## Create a new interpreted view
-######
-
-# Convert the PanCanAtlas data to the same views
-generate.feature.data.summary(gene.sample.data, feature.gene.matrix, type = 'expression', value = 'median', num.gene.threshold = 5)
-
+## TODO: R reads these as plot() functions for data types llv and cv. Would like to update my code to make that work!
+plot.llv( fn.labs, 'platypus_output' ) # Plot label learning
+plot.cv(fn.labs, 'platypus_output') # Plot cross validation - these plots pair with the llv plot
