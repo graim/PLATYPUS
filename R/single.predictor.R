3021d44759a069e4f4c342a635c1ccfeddf1dde8
Kiley Graim <kgraim@gmail.com>
2019-04-23 15:59:26 -0400

    Updated input options, switched to using caret for all model types

--- R/single.predictor.R
+++ R/single.predictor.R
@@ -1,13 +1,17 @@
 ### single support vector machine predictor #######################################################
-# generalized function to take in feature by sample expression matrix and predict drug sensitivity
+# generalized function get best parameters for a given task/view pair
 ###################################################################################################
 #fuck it, using caret and a generic function for it. 
 #' Parameter sweeps of svm, random forest, and elastic net models for config generation.
 #'
 #' @param X Feature matrix (sample per row)
 #' @param y Named vector of labels
+#' @param model Type of single predictor model to build. Choose from en (elastic net), rf (random forest), or svm (support vector machine)
+#' @param ignore.label Label class to ignore. Will be removed from labels set before training.
 #'
 #' @return list containing optimal model summary
+#'
+#' @import caret
 #'
 #' @examples
 #' X <- matrix(rnorm(10000), nrow=100)
@@ -37,14 +41,19 @@ single.predictor <- function(X,y,model,ignore.label='intermediate') {
   X   <- X[intersect(rownames(X),names(y)),,drop=F]
   y   <- y[intersect(rownames(X),names(y))]
 
-  ## Make sure we're using factor labels
-  if(!is.factor(y)) {
-    message('Converting labels to factor')
-    y <- as.factor(y)
-  }
+  y   <- factor(y) # Remove the excess factor level ignore.label
+
+#  ## Make sure we're using factor labels
+#  if(!is.factor(y)) {
+#    message('Converting labels to factor')
+#    y <- as.factor(y)
+#  }
  
   ## Parameter sweep and return 'best' model
-  res.model <- caret::train(x = t(X), y = y, method=model, preProcess = 'center', metric = 'Accuracy', trControl = trainControl(method = 'repeatedcv', number = 10))
-  return(res.model$results[which(res.model$results$Accuracy == max(res.model$results$Accuracy))[1],])
+  res.model <- caret::train(x = t(X), y = y, method=model, preProcess = 'center', metric = 'Accuracy', trControl = caret::trainControl(method = 'repeatedcv', number = 10))
+  res <- res.model$results[which(res.model$results$Accuracy == max(res.model$results$Accuracy))[1],]
+  res$Model <- model # For gen.config use
+  return( res )
+#  return(res.model$results[which(res.model$results$Accuracy == max(res.model$results$Accuracy))[1],])
 
 }
